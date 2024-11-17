519bf79d35b8fb8276850859d11ce1265bc65e26
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2024-11-17 23:46:45 +0300

    Update script3.2.sql

--- folder2.2/script3.2.sql
+++ folder2.2/script3.2.sql
@@ -1,23 +1,39 @@
-WITH ArtistGenreCount AS (
-    SELECT 
-        aa.album_id,
-        aa.artist_id,
-        COUNT(DISTINCT ag.genre_id) AS genre_count
-    FROM 
-        Artist_Album aa
-    JOIN 
-        Artist_Genre ag ON aa.artist_id = ag.artist_id
-    GROUP BY 
-        aa.album_id, aa.artist_id
-)
+CREATE TABLE Artist (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
 
-SELECT 
-    a.title AS album_title
-FROM 
-    Album a
-JOIN 
-    ArtistGenreCount agc ON a.id = agc.album_id
-GROUP BY 
-    a.id, a.title
-HAVING 
-    COUNT(DISTINCT agc.genre_count) > 1;
+CREATE TABLE Genre (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Album (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    release_year INT NOT NULL CHECK (release_year >= 1900 AND release_year <= EXTRACT(YEAR FROM CURRENT_DATE))
+);
+
+CREATE TABLE Track (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    duration INTERVAL NOT NULL,
+    album_id INT REFERENCES Album(id)
+);
+
+CREATE TABLE Compilation (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Artist_Album (
+    artist_id INT REFERENCES Artist(id),
+    album_id INT REFERENCES Album(id),
+    PRIMARY KEY (artist_id, album_id)
+);
+
+CREATE TABLE Artist_Genre (
+    artist_id INT REFERENCES Artist(id),
+    genre_id INT REFERENCES Genre(id),
+    PRIMARY KEY (artist_id, genre_id)
+);
