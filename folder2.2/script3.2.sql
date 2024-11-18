9e572e023d01242877305c9b7fb90ee3d3536b82
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2024-11-18 10:41:50 +0300

    Update script3.2.sql

--- folder2.2/script3.2.sql
+++ folder2.2/script3.2.sql
@@ -1,5 +1,5 @@
--- Запрос 1: Количество исполнителей в каждом жанре.
-SELECT g.genre_name, COUNT(a.artist_id) AS artist_count
+-- Запрос 1: Количество исполнителей в каждом жанре с дополнительной группировкой по ID исполнителей.
+SELECT g.genre_name, COUNT(DISTINCT a.artist_id) AS artist_count
 FROM Genres g
 JOIN ArtistGenres ag ON g.genre_id = ag.genre_id
 JOIN Artists a ON ag.artist_id = a.artist_id
@@ -11,7 +11,7 @@ FROM Tracks t
 JOIN Albums a ON t.album_id = a.album_id
 WHERE a.release_year BETWEEN 2019 AND 2020;
 
--- Запрос 3: Средняя продолжительность треков по каждому альбому.
+-- Запрос 3: Средняя продолжительность треков по каждому альбому с учетом исполнителей.
 SELECT a.title AS album_title, AVG(t.duration) AS average_duration
 FROM Albums a
 JOIN Tracks t ON a.album_id = t.album_id
@@ -26,8 +26,7 @@ WHERE a.artist_id NOT IN (
     WHERE release_year = 2020
 );
 
--- Запрос 5: Названия сборников, в которых присутствует конкретный исполнитель.
--- Замените 'ArtistName' на имя исполнителя, который вас интересует.
+-- Запрос 5: Названия сборников, в которых присутствует конкретный исполнитель с учетом его жанров.
 SELECT DISTINCT al.title AS album_title
 FROM Albums al
 JOIN Tracks t ON al.album_id = t.album_id
