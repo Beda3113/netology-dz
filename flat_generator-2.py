f76af3c0fdf7cc79743bab159467baa9fb9ac858
Beda3113 <130255340+Beda3113@users.noreply.github.com>
2025-01-03 22:24:25 +0300

    Add files via upload

---
+++ flat_generator-2.py
@@ -0,0 +1,27 @@
+def flat_generator(list_of_lists):
+    for sublist in list_of_lists:
+        if isinstance(sublist, list):
+            yield from flat_generator(sublist)
+        else:
+            yield sublist
+
+
+def test_4():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
